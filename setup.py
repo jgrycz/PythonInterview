ba46a08bae9dee3868c8521c5350c65cbdd1483d
Jaroslaw Grycz <jaroslaw.grycz@gmail.com>
2018-02-17 22:00:29 +0100

    Added missing files

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+:author: Jaroslaw Grycz
+:contact: jaroslaw.grycz@gmail.com
+"""
+from setuptools import setup
+
+
+setup(name='PythonInterveiw',
+      version='0.1',
+      author='Jaroslaw Grycz',
+      author_email='jaroslaw.grycz@gmail.com')
---
+++ test/test_task_05.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+:author: Jaroslaw Grycz
+:contact: jaroslaw.grycz@gmail.com
+"""
