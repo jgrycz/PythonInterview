08a70293df28a0b56fe866fc4be6c9b38bb1039e
jgrycz <jaroslaw.grycz@gmail.com>
2019-06-02 23:55:36 +0200

    Create task_17.py

---
+++ PythonInterview/task_17.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+:author: Jaroslaw Grycz
+:contact: jaroslaw.grycz@gmail.com
+"""
+#1. Napisz funkcję która na wejściu dostaje macierz NxN i wyświetla ją w "spiralny sposób": https://graphicheck.com/image/cache/catalog/ad/spiral-assets-affinity-designer-500x500.png
